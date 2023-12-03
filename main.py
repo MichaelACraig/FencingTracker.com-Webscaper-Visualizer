235b0e7aa20373aeba989181c508fb18bf25a54e
Michael Craig <96352681+MichaelACraig@users.noreply.github.com>
2023-12-02 19:03:39 -0500

    Complete up to 997 people; Bugfix pullLargestClubs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import csv
 import pandas
 import requests
+import os
 
 #Pulls the name data from the top 50 clubs on FencingTracker
 def pullLargestClubs(outputCSV):
@@ -12,7 +13,6 @@ def pullLargestClubs(outputCSV):
     url = "https://www.fencingtracker.com/largestclubs"
     finalLinks = []
 
-    
     #Layered hyperlinks from the starting URL. Below shows hyperlink path for data collection:
     #Largest Clubs -> Club's rating -> Club's Members -> Individual Member Info -> /history to access Bout History
     # 4 Hyperlinks to move through Club's Members needs to loop through all members
@@ -36,104 +36,118 @@ def pullLargestClubs(outputCSV):
             href = ratingLink.get('href')
 
             #Cleaning function; removes the /topclubs/... hyperlink since it is a repetitive call; Will pretty much loop forever if branched down
-            if href == "/topclubs/epee" or href == "/topclubs/foil" or href == "/topclubs/epee" or href == "/topclubs/saber":
+            if href == "/topclubs/epee" or href == "/topclubs/foil" or href == "/topclubs/saber" or href == "#":
                 continue
 
             #Third Layer; Names Page from Club Ratings Page
-            clubMemebersPage = "https://fencingtracker.com" + href
-            pageResponse = requests.get(clubMemebersPage, headers=headers)
-            membersSoup = BeautifulSoup(pageResponse.text, "html.parser")
+            if href is not None:
+                clubMemebersPage = "https://fencingtracker.com" + href
+                pageResponse = requests.get(clubMemebersPage, headers=headers)
+                membersSoup = BeautifulSoup(pageResponse.text, "html.parser")
 
-            membersLinks = membersSoup.find_all('a')
+                membersLinks = membersSoup.find_all('a')
 
-            for memberLink in membersLinks:
-                href = memberLink.get('href')
+                for memberLink in membersLinks:
+                    href = memberLink.get('href')
+                    print(str(href))
 
-                if href is not None and href.startswith("/p/"):
-                    finalLinks.append(href)
+                    if href is not None and href.startswith("/p/"):
+                        finalLinks.append(href)
+                        length = len(finalLinks)
+                        print(str(length))
 
-            #FOURTH LAYER IS UNNECESSARY SINCE WE CAN JUST APPEND /history LATER
+                #FOURTH LAYER IS UNNECESSARY SINCE WE CAN JUST APPEND /history LATER
 
-            #Output to CSV file
-            with open(outputCSV, 'w', newline='') as csvfile:
-                        writer = csv.writer(csvfile)
-                        writer.writerow(['URL TAG'])
-                        for link in finalLinks:
-                             writer.writerow([link])
+                #Output to CSV file
+    with open(outputCSV, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['URL TAG'])
+        for link in finalLinks:
+            writer.writerow([link])
             
 
 
 #Main scraping method; Pulls URLs in .csv created in pullLinks to generate data from FencingTracker
-def scrapeAndExport(url, outputFile):
+def scrapeAndExport(inputFile, url, outputFile):
     #Header element necessary for website access
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     }
 
-    #BeautifulSoup Basic Script
-    result = requests.get(url, headers=headers)
-    doc = BeautifulSoup(result.text, "html.parser")
+    with open(inputFile, 'r') as csvfile:
+        csvReader = csv.reader(csvfile)
+        next(csvReader)
+        
+        for row in csvReader:
 
-    #Variables for easier assignment
-    #Pulls NAME; Finds the first item labeled under h1
-    NAME = doc.find('h1')
+            rowContent = ''.join(row)
+            urlExtension = url + rowContent + "/history"
+            print(urlExtension)
 
-    #Pulls CLUB AFFILIATIONS
-    CLUBS = doc.find('h2')
+            #BeautifulSoup Basic Script
+            result = requests.get(urlExtension, headers=headers)
+            doc = BeautifulSoup(result.text, "html.parser")
 
-    #Pulls Win/Loss Statistics Table
-    statsTable = doc.find('table')
+            #Variables for easier assignment
+            #Pulls NAME; Finds the first item labeled under h1
+            NAME = doc.find('h1')
 
-    #Stats Variable Creation
-    rows = statsTable.find_all('tr')
+            #Pulls CLUB AFFILIATIONS
+            CLUBS = doc.find('h2')
 
-    ATWins = ""
-    ATLosses = ""
-    winRatio = ""
-    poolWins = ""
-    poolLosses = ""
-    poolWinRatio = ""
-    DEWins = ""
-    DELosses = ""
-    DEWinRatio = ""
+            #Pulls Win/Loss Statistics Table
+            statsTable = doc.find('table')
 
-    #Placeholders for the stats; Shows variable to index representation
-    statsArray = [
-        NAME.text, #.text is needed to remove HTML tags
-        CLUBS.text,
-        ATWins,
-        ATLosses,
-        winRatio,
-        poolWins,
-        poolLosses,
-        poolWinRatio,
-        DEWins,
-        DELosses,
-        DEWinRatio
-    ]
+            #Stats Variable Creation
+            rows = statsTable.find_all('tr')
 
-    #Proper parsing for all Variables; Populates statsArray
-    index = 2
-    for row in rows:
-        cells = row.find_all('td')
-        if cells:
-            last_cell = cells[-1]
-            statsArray[index] = last_cell.text
-            index += 1
+            ATWins = ""
+            ATLosses = ""
+            winRatio = ""
+            poolWins = ""
+            poolLosses = ""
+            poolWinRatio = ""
+            DEWins = ""
+            DELosses = ""
+            DEWinRatio = ""
 
-    print("Appending is complete")
-    #Reset index for tidiness
-    index = 0
+            #Placeholders for the stats; Shows variable to index representation
+            statsArray = [
+                NAME.text, #.text is needed to remove HTML tags
+                CLUBS.text,
+                ATWins,
+                ATLosses,
+                winRatio,
+                poolWins,
+                poolLosses,
+                poolWinRatio,
+                DEWins,
+                DELosses,
+                DEWinRatio
+            ]
 
-    #.csv file creation
-    with open(outputFile, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        if csvfile.tell() == 0:
-            writer.writerow(['NAME', 'CLUBS', 'ATWins', 'ATLosses', 'winRatio', 'poolWins', 'poolLosses', 'poolWinRatio', 'DEWins', 'DELosses', 'DEWinRatio'])
-        writer.writerow(statsArray)
+            #Proper parsing for all Variables; Populates statsArray
+            index = 2
+            for row in rows:
+                cells = row.find_all('td')
+                if cells:
+                    last_cell = cells[-1]
+                    statsArray[index] = last_cell.text
+                    index += 1
+
+            print("Appending is complete")
+            #Reset index for tidiness
+            index = 0
+
+            #.csv file creation
+            with open(outputFile, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                if os.path.getsize(outputFile) == 0:
+                    writer.writerow(['NAME', 'CLUBS', 'ATWins', 'ATLosses', 'winRatio', 'poolWins', 'poolLosses', 'poolWinRatio', 'DEWins', 'DELosses', 'DEWinRatio'])
+                writer.writerow(statsArray)
 
 #Example Call 
-#scrapeAndExport('https://www.fencingtracker.com/p/100309761/Johnathan-Ballou/history',
-#        '/Users/michelecraig/Desktop/My Project Files/Python/FencingTrackerCSVOutputs/output.csv')
+scrapeAndExport('/Users/michelecraig/Desktop/My Project Files/Python/FencingTrackerCSVOutputs/URLInputs.csv', 'https://www.fencingtracker.com', '/Users/michelecraig/Desktop/My Project Files/Python/FencingTrackerCSVOutputs/output.csv')
 
-pullLargestClubs('/Users/michelecraig/Desktop/My Project Files/Python/FencingTrackerCSVOutputs/URLInputs.csv')
+#pullLargestClubs('/Users/michelecraig/Desktop/My Project Files/Python/FencingTrackerCSVOutputs/URLInputs.csv')
+#print("complete")
