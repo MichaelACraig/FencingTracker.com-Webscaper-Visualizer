81e2dee3a4eb9462f03ae8fac6b24e4fb5136cbe
Michael Craig <michaelcraigcoding@gmail.com>
2024-01-24 14:03:47 -0500

    Continuing work on Library linking for Maps

--- analysis.r
+++ analysis.r
@@ -5,15 +5,57 @@ head(RegionData)
 TwoClubFencers <- read.csv("Outputs/TwoClubFencers.csv")
 head(TwoClubFencers)
 
-# Visualization of Regional Data through the US Map; Installed needed packages
-library("leaflet")
-library("htmlwidgets")
-library("ggplot2")
-library("maps")
-library("sf")
+#print(.libPaths())
+# Load the necessary packages
+library(leaflet)
+library(magrittr)
+library(sf)
+library(geojsonio)
 
+# Read the GeoJSON file into R
+# Replace 'us_states.geojson' with the path to your file
+us_states <- sf::st_read('/../../../Library/R/x86_64/4.3/library/geojsonio/examples/us_states.json')
 
+# Create a leaflet map
+m <- leaflet() %>%
+    addTiles()
 
-#Saves map to html to be viewed and opened in browser
+# Add polygons for each region
+for (state in Midwest) {
+    m <- m %>%
+        addPolygons(data = us_states[us_states$NAME == state, ], 
+                    fillColor = "blue", 
+                    fillOpacity = 0.6, 
+                    color = "white", 
+                    weight = 1)
+}
+
+for (state in West) {
+    m <- m %>%
+        addPolygons(data = us_states[us_states$NAME == state, ], 
+                    fillColor = "red", 
+                    fillOpacity = 0.6, 
+                    color = "white", 
+                    weight = 1)
+}
+
+for (state in South) {
+    m <- m %>%
+        addPolygons(data = us_states[us_states$NAME == state, ], 
+                    fillColor = "green", 
+                    fillOpacity = 0.6, 
+                    color = "white", 
+                    weight = 1)
+}
+
+for (state in North) {
+    m <- m %>%
+        addPolygons(data = us_states[us_states$NAME == state, ], 
+                    fillColor = "yellow", 
+                    fillOpacity = 0.6, 
+                    color = "white", 
+                    weight = 1)
+}
+
+# Display the map
 saveWidget(m, "map.html", selfcontained = TRUE)
-
